0024ce23a00b38d9415c81fa4055af8a47ecdece
Jess Casas <jess.casas.dev@gmail.com>
2024-02-13 07:30:55 -0500

    feat-7: Add Role Validations. closes #7

--- src/main/java/com/spring/study/university/University/services/RoleService.java
+++ src/main/java/com/spring/study/university/University/services/RoleService.java
@@ -1,12 +1,10 @@
 package com.spring.study.university.University.services;
 
 import com.spring.study.university.University.domain.Role;
-import com.spring.study.university.University.enums.RoleEnum;
 import com.spring.study.university.University.repositories.RoleRepository;
+import com.spring.study.university.University.services.validations.RoleValidations;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 
 @Service
@@ -14,17 +12,11 @@ import org.springframework.web.server.ResponseStatusException;
 public class RoleService {
 
   private final RoleRepository roleRepository;
+  private final RoleValidations roleValidations;
 
   public Role saveRole(Role role) {
-    if (!role.getRole().name().equals(RoleEnum.Student.name())
-        && !role.getRole().name().equals(RoleEnum.Professor.name())
-        && !role.getRole().name().equals(RoleEnum.Admin.name())) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-    }
-
-    if(roleRepository.findByRole(role.getRole()).isPresent()){
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role already exists");
-    }
+    roleValidations.validateRolIsValid(role);
+    roleValidations.validateIfRoleNoExists(role);
     return roleRepository.save(role);
   }
 }
